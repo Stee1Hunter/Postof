ade66e689a7f0dded5aa7a81ab3355ad591307fa
Stee1Hunter <144276556+Stee1Hunter@users.noreply.github.com>
2025-04-11 15:01:06 +0300

    Update Config.java

--- app/src/main/java/com/example/postof/Config.java
+++ app/src/main/java/com/example/postof/Config.java
@@ -1,5 +1,5 @@
 package com.example.postof;
 public class Config {
-    public static final String EMAIL ="isip_a.a.provarenov@mpt.ru";
-    public static final String PASSWORD ="Provarenov0905";
+    public static final String EMAIL ="***";
+    public static final String PASSWORD ="****";
 }
